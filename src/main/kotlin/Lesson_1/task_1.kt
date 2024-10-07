2d62715095f05f61d3a6af12a9722e0be8cd4b1b
IamLoveEBOLA <noobsaibottrolo@gmail.com>
2024-10-07 21:22:00 +0300

    project-refacrot

---
+++ src/main/kotlin/Lesson_1/task_1.kt
@@ -0,0 +1,9 @@
+package org.example.Lesson_1
+
+fun main() {
+    val orderCount: Int = 75
+    val thanksMessage: String = "Спасибо за покупку!"
+
+    println("общее число заказов $orderCount , $thanksMessage")
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,5 @@
 package org.example
 
 fun main() {
-    val orderCount: Int = 75
-    val thanksMessage: String = "Спасибо за покупку!"
-
-    println("общее число заказов $orderCount , $thanksMessage")
-
-
-    //println("Hello World!")
-
+    println("Hello World!")
 }
